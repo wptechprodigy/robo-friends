1d57c6f0f914ea65307144ca0d8602abdf47fedb
Waheed Afolabi <waheed.freelancer16@gmail.com>
2019-02-07 21:06:54 +0100

    Updates

---
+++ precache-manifest.32f7acd6a1432bcbe79713d2611bdb6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4277a02e4e953526af58",
+    "url": "/static/js/main.4277a02e.chunk.js"
+  },
+  {
+    "revision": "4506554a467bab318198",
+    "url": "/static/js/1.4506554a.chunk.js"
+  },
+  {
+    "revision": "4277a02e4e953526af58",
+    "url": "/static/css/main.1221ed9d.chunk.css"
+  },
+  {
+    "revision": "4506554a467bab318198",
+    "url": "/static/css/1.97a592bb.chunk.css"
+  },
+  {
+    "revision": "b7f9e3e39fa9e912ef6e22ea934fc720",
+    "url": "/index.html"
+  }
+];
