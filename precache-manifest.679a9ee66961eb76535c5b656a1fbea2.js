3781c5e61fcba8a3fe47648ad404e0526e56f26c
wptechprodigy <waheed.freelancer16@gmail.com>
2020-02-10 19:41:17 +0100

    Updates

---
+++ precache-manifest.679a9ee66961eb76535c5b656a1fbea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "da06e6f6a48caf6eeb62",
+    "url": "/robo-friends/static/css/main.b8e43e9c.chunk.css"
+  },
+  {
+    "revision": "da06e6f6a48caf6eeb62",
+    "url": "/robo-friends/static/js/main.da06e6f6.chunk.js"
+  },
+  {
+    "revision": "a3357e14acf20768f4a2",
+    "url": "/robo-friends/static/css/1.2cb299dc.chunk.css"
+  },
+  {
+    "revision": "a3357e14acf20768f4a2",
+    "url": "/robo-friends/static/js/1.a3357e14.chunk.js"
+  },
+  {
+    "revision": "617e3c50e67378d4af10",
+    "url": "/robo-friends/static/js/runtime~main.617e3c50.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "7d7927eb55a04196704a4c1358033493",
+    "url": "/robo-friends/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32f7acd6a1432bcbe79713d2611bdb6b.js"
+  "/robo-friends/precache-manifest.679a9ee66961eb76535c5b656a1fbea2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/robo-friends/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
